4acf67434a563d4b285e2756d81e6a19f9c3d5f5
江文得 <82697451@qq.com>
2016-07-27 16:31:29 +0800

    默认关闭水印功能

--- data/mark/inc_photowatermark_config.php
+++ data/mark/inc_photowatermark_config.php
@@ -1,11 +1,11 @@
 <?php
-$photo_markup = '1';
-$photo_markdown = '1';
+$photo_markup = '0';
+$photo_markdown = '0';
 $photo_marktype = '1';
 $photo_wwidth = '120';
 $photo_wheight = '120';
 $photo_waterpos = '9';
-$photo_watertext = 'www.dedecms.com';
+$photo_watertext = '';
 $photo_fontsize = '20';
 $photo_fontcolor = '0,0,0';
 $photo_marktrans = '100';
